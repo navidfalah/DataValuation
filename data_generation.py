86d5e13813e4faed95fb2b5dccfe50ddd0c1ab8b
navid <navid.falah7@gmail.com>
2024-02-11 16:34:20 +0330

    data generation updated

--- data_generation.py
+++ data_generation.py
@@ -1,5 +1,9 @@
 import numpy as np
 
-def generate_synthetic_data(covariance_matrix, n_samples=1000):
+def generate_synthetic_data(covariance_matrix, n_samples=1000, random_state=None):
+    if random_state is not None:
+        np.random.seed(random_state)
+
     mean = np.zeros(covariance_matrix.shape[0])
-    return np.random.multivariate_normal(mean, covariance_matrix, n_samples)
+    synthetic_data = np.random.multivariate_normal(mean, covariance_matrix, n_samples)
+    return synthetic_data
