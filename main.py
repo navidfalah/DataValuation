e59972ee2386883caaf1c76cf2df17c086cafefd
navid <navid.falah7@gmail.com>
2024-02-11 16:12:05 +0330

    main file added

---
+++ main.py
@@ -0,0 +1,27 @@
+import numpy as np
+from data_generation import generate_synthetic_data
+from covariance_analysis import calculate_eigenvalues, calculate_diversity, calculate_relevance
+from visualization import plot_data_distribution, plot_eigenvalues
+
+
+buyer_covariance_matrix = np.array([[1, 0.1], [0.1, 0.25]])
+seller_covariance_matrix = np.array([[0.5, 0.1], [0.1, 0.5]])
+
+buyer_data = generate_synthetic_data(buyer_covariance_matrix)
+seller_data = generate_synthetic_data(seller_covariance_matrix)
+
+buyer_eigvals, buyer_eigvecs = calculate_eigenvalues(buyer_data)
+seller_eigvals, seller_eigvecs = calculate_eigenvalues(seller_data)
+
+diversity = calculate_diversity(buyer_eigvals, seller_eigvals)
+relevance = calculate_relevance(buyer_eigvals, seller_eigvals)
+
+print(f"Buyer Data Eigenvalues: {buyer_eigvals}")
+print(f"Seller Data Eigenvalues: {seller_eigvals}")
+print(f"Diversity: {diversity}")
+print(f"Relevance: {relevance}")
+
+plot_data_distribution(buyer_data, "Buyer Data Distribution")
+plot_data_distribution(seller_data, "Seller Data Distribution")
+plot_eigenvalues(buyer_eigvals, "Buyer Eigenvalues")
+plot_eigenvalues(seller_eigvals, "Seller Eigenvalues")
